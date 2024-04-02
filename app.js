084f914f4771381ca216ceafff36b149c8fd6214
Brian Brown <brianbrown2002@gmail.com>
2024-04-02 13:38:43 -0400

    render fix

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ import express from "express";
 import Hello from "./Hello.js";
 import lab5 from "./lab5.js";
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Hello(app);
 lab5(app);
 
