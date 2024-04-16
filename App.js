3b8b944a3ab15a0d4522fbce5eb76dcd2e1b0928
Brian Brown <brianbrown2002@gmail.com>
2024-04-16 15:40:47 -0400

    fixed stuff

--- App.js
+++ App.js
@@ -9,8 +9,8 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import UserRoutes from "./Users/routes.js";
 
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
-const app = express();
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+mongoose.connect(CONNECTION_STRING);const app = express();
 app.use(
     cors({
       credentials: true,
