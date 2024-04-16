4583c7ac920997a4f5a476df300a9686da8435b7
Brian Brown <brianbrown2002@gmail.com>
2024-04-16 14:50:17 -0400

    kinda works

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
 import express from "express";
+import session from "express-session";
+import "dotenv/config";
 import Hello from "./Hello.js";
 import lab5 from "./lab5.js";
 import cors from "cors";
+import mongoose from "mongoose"
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from "./Users/routes.js";
 
-
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+   );
+   const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 Hello(app);
 lab5(app);
+UserRoutes(app)
 
 app.listen(process.env.PORT || 4000);
